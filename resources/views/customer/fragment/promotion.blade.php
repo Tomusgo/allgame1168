267bf62276933b1d2bdde45e2e057834c24d30bb
tomus <ph7185150@gmail.com>
2022-01-21 16:36:18 +0700

    clean

--- resources/views/customer/fragment/promotion.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="row">
-    @foreach ($promotions as $p)
-    <div class="col-4" style="padding: 2px;">
-        <img class="btn-image-modal w-100 img-fluid promotion-img" src="{{ $p['image'] }}" alt="{{ $p['name'] }}">
-    </div>
-    @endforeach
-</div>
